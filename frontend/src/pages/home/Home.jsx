8285bc0da635ec5c4194ea4af58017a4c97904ee
mohamedInfath <mohamedinfath99@gmail.com>
2023-01-18 15:58:45 +0530

    featured type defined

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import Featured from '../../components/featured/Featured'
 import Navbar from '../../components/navbar/Navbar'
 import './Home.scss'
 
@@ -5,90 +6,7 @@ const Home = () => {
   return (
     <div className='home'>
       <Navbar />
-
-      <img width="100%" src='https://eldiario.com/wp-content/uploads/2021/11/619306-bg-full-netflix-grid-v2.desktop.jpg' alt='cover' />
-
-
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem pariatur blanditiis, minus odio quisquam, velit sunt, autem dicta consectetur tempore laboriosam eos omnis accusantium aut nobis dignissimos non. Delectus, autem!
-
+      <Featured type='movie' />
     </div>
   )
 }
